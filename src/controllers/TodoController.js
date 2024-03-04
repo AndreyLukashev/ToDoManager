8467227a6df2cd8e45af518ce76eea675d697e29
AndreyLukashev <energy2024@internet.ru>
2024-03-04 11:33:14 +0300

    add header and task

---
+++ src/controllers/TodoController.js
@@ -0,0 +1,5 @@
+export class TodoController {
+    constructor(models, view) {
+        
+    }
+}
---
+++ src/views/TodoView.js
@@ -0,0 +1,34 @@
+import { View } from './View';
+
+export class TodoView extends View {
+
+    render({todoModel}) {
+        const{data} = todoModel;
+        return `
+        <div class="mt-16">
+          <div class="flex justify-between items-center">
+            <p class="text-sky-500">Total Tasks ${data.length}</p>
+            <button class="bg-sky-500 p-2 text-white rounded">Create Task</button>
+          </div>
+
+          <div class="mt-5 flex flex-col gap-6">
+            <div class="rounded bg-slate-700 p-2 border-slate-500">
+              <div class="flex justify-between items-center">
+                <div class="flex gap-2">
+                  <input type = "checkbox" />
+                  <h3 class="text-lg text-white line-clamp-2">Title</h3>
+                </div>
+                
+                <div class="flex gap-2">
+                  <button class="bg-sky-500 px-2 rounded text-white">Edit</button>
+                  <button class="bg-red-500 px-2 rounded text-white">Delete</button>
+                </div>
+              </div>
+              <p class="text-sm text-white line-clamp-2">Lorem ipsum</p>
+            </div>
+          </div>
+
+        </div>
+        `
+    }
+}
